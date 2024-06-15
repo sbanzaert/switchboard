9286f3a334ed84759e52f04480e3689914dacaec
sbanzaert <sgtist@mit.edu>
2024-06-14 22:32:19 -0400

    scoring

--- MIDI-LEDtest.py
+++ MIDI-LEDtest.py
@@ -8,19 +8,26 @@ m=MidiFile(midPath)
 midParsed = mid_parser.MidiFile(midPath)
 g = (i for i, e in enumerate(midParsed.instruments) if e.name=="Game")
 gameTrack = next(g) + 1
+checkNote = 120
+score = 100
 
 jackState = ["grey", "grey", "grey", "grey"]
 jackNoteCenters = [0, 1, 2, 3]
+jackTargets = [0,0,0,0] # 0 = nop, 1 = actuated
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 outPins = (4, 17, 23, 24, 6, 12, 13, 16)
 grPins = (4, 23, 6, 13)
 rPins = (17, 24, 12, 16)
+inPins = (27, 22, 8, 5)
 
 for i in outPins:
     GPIO.setup(i, GPIO.OUT)
     GPIO.output(i, 0)    
+
+for i in inPins:
+    GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def updateScreen():
     for i in range(len(jackState)):
@@ -30,7 +37,14 @@ def updateScreen():
             GPIO.output(rPins[i], 1)
         if (jackState[i] == "green"):
             GPIO.output(grPins[i], 1)
-    
+
+def updateScore():
+    for i in range(len(jackTargets)):
+        if (GPIO.input(inPins[i] == jackTargets[i])):
+            score += 1
+        else:
+            score -= 1
+
 mout = rtmidi.MidiOut()
 ports = mout.get_ports()
 print(ports)
@@ -42,13 +56,21 @@ for msg in m.play():
         mout.send_message(msg.bytes())
     else:
         for i in range(len(jackState)):
-            if (msg.type== "note_off" and (msg.note == jackNoteCenters[i] or msg.note == jackNoteCenters[i]+30 or msg.note == jackNoteCenters[i]+60)):
+            if (msg.type == "note_off" and msg.note == jackNoteCenters[i]):
                 jackState[i] = "grey"
-            elif (msg.type == "note_on" and msg.note == jackNoteCenters[i]+1):
+                jackTargets[i] = 0
+            elif (msg.type == "note_off" and msg.note == jackNoteCenters[i]+30):
+                jackState[i] = 'grey'
+            elif (msg.type == "note_off" and  msg.note == jackNoteCenters[i]+60):
+                jackState[i] = 'grey'   
+            elif (msg.type == "note_on" and msg.note == jackNoteCenters[i]+60):
                 jackState[i] = "red"
+            elif (msg.type == "note_on" and msg.note == jackNoteCenters[i]+30):
+                jackState[i] = "green"
             elif (msg.type == "note_on" and msg.note == jackNoteCenters[i]):
                 jackState[i] = "green"
-            elif (msg.type == "note_on" and msg.note == jackNoteCenters[i]-1):
-                jackState[i] = "red"
+                jackTargets[i] = 1
+        if (msg.type == "note_on" and msg.note == checkNote):
+            updateScore()
         updateScreen()
 GPIO.cleanup()
