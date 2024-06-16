7b5d595fcbfbaf9fa7c66827b84b66443b419fcc
sbanzaert <sgtist@mit.edu>
2024-06-16 19:34:15 -0400

    shut up pins and midi

--- shutupMIDI.py
+++ shutupMIDI.py
@@ -1,10 +1,21 @@
 from __future__ import print_function
-
+import RPi.GPIO as GPIO
 import time
 
 import rtmidi
 from rtmidi.midiconstants import (ALL_SOUND_OFF, CONTROL_CHANGE,
                                   RESET_ALL_CONTROLLERS)
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+outPins = (4, 17, 23, 24, 6, 12, 13, 16)
+grPins = (4, 23, 6, 13)
+rPins = (17, 24, 12, 16)
+inPins = (27, 22, 8, 5)
+
+for i in outPins:
+    GPIO.setup(i, GPIO.OUT)
+    GPIO.output(i, 0)    
 
 
 midiout = rtmidi.MidiOut()
