4734c1d6fa15ba75b80d0eeecab4d6ceb30de6be
sbanzaert <sgtist@mit.edu>
2024-12-08 23:30:26 -0500

    sketch call output

---
+++ playPhoneCall.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+# from https://spotify.github.io/pedalboard/faq.html
+
+from pedalboard import Pedalboard, Chorus, Reverb
+# likely plugins: chorus, reverb, lowpass, bitcrush
+from pedalboard.io import AudioFile
+from tqdm import tqdm
+
+board = Pedalboard([Compressor(), Reverb()])
+reverb = board[1]
+
+# Smaller step sizes would give a smoother transition,
+# at the expense of processing speed
+step_size_in_samples = 100
+
+# Manually step through the audio _n_ samples at a time, reading in chunks:
+with AudioFile("sample.wav") as af:
+
+    # Open the output audio file so that we can directly write audio as we process, saving memory:
+    with AudioFile(
+        "sample-processed-output.wav", "w", af.samplerate, num_channels=af.num_channels
+    ) as o:
+
+        # Create a progress bar to show processing speed in real-time:
+        with tqdm(total=af.frames, unit=' samples') as pbar:
+            for i in range(0, af.frames, step_size_in_samples):
+                chunk = af.read(step_size_in_samples)
+
+                # Set the reverb's "wet" parameter to be equal to the
+                # percentage through the track (i.e.: make a ramp from 0% to 100%)
+                percentage_through_track = i / af.frames
+                reverb.wet_level = percentage_through_track
+
+                # Update our progress bar with the number of samples received:
+                pbar.update(chunk.shape[1])
+
+                # Process this chunk of audio, setting `reset` to `False`
+                # to ensure that reverb tails aren't cut off
+                output = board.process(chunk, af.samplerate, reset=False)
+                o.write(output)
+
