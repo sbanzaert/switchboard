a7fdcb46f9a8a4c05f9928d23d4a85046f759acc
Steve B <sgtist@mit.edu>
2024-12-21 18:09:01 -0500

    solved dsound konundrum

--- midPassthroughTest.py
+++ midPassthroughTest.py
@@ -11,7 +11,7 @@ m = MidiFile('/home/pi/Python/switchboard/midi/peeweeswitchtest.mid')
 mout = rtmidi.MidiOut()
 ports = mout.get_ports()
 print(ports)
-mout.open_port(1)
+mout.open_port(2)
 
 for msg in m.play():
     if(msg.channel != 15):
